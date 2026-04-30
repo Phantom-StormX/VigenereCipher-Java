4c59016ccc279a0b701fcaa8b2d379f6dab468ad
impichler <impichler@thewildcats.net>
2026-04-30 09:36:31 -0400

    @ElliottandCoachGeorge, lmk if this works

--- src/main/java/com/phantomstormx/vigenerecipherjava/Main.java
+++ src/main/java/com/phantomstormx/vigenerecipherjava/Main.java
@@ -2,18 +2,20 @@ package com.phantomstormx.vigenerecipherjava;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.util.Objects;
 
-public class HelloApplication extends Application {
+// combines all classes together, then launcher uses start to start the app
+
+public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Vigenere-view.fxml"))); // yoinks all the information from the fxml (includes the control, idk how tho don't ask me)
+        primaryStage.setTitle("Vigenere Cipher"); // sets actual app title
+        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.show();
     }
 }
